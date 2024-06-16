92da4242c063d5979fe0d84b7106917acce89d3b
Parth Gupta <parthgupta3003@gmail.com>
2024-06-16 06:09:00 +0530

    optimisations and refactors

--- gpt_utils.py
+++ gpt_utils.py
@@ -7,19 +7,16 @@ load_dotenv()
 client = Groq(
     api_key=os.getenv("GROQ_API_KEY"),
 )
-
+system_prompt=[{"role": "system","content": "Stricly limit response under 2000 characters. You are a Discord Bot built to provide information and have friendly conversations. Sound informal like a friend"}]
+messages=[]
 def gpt_response(message):
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "system",
-                "content": "Stricly limit response under 2000 characters.",
-            },
-            {
-                "role": "user",
-                "content": message,
-            }
-        ],
-        model="llama3-8b-8192",
-    )
-    return chat_completion.choices[0].message.content
+    global messages
+    if len(messages)>20:
+        messages.pop(0)
+        messages.pop(0)
+    messages.append({"role": "user", "content" : message})
+    response = client.chat.completions.create(model="llama3-70b-8192",
+        messages = system_prompt+messages
+        )
+    messages.append({"role": "assistant", "content" : response.choices[0].message.content})
+    return response.choices[0].message.content
